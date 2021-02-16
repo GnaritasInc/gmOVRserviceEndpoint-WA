c24ee044a0d681cf053607a8bc8ff0baff81ed8f
Christopher <cwisdo@gnaritas.com>
2021-02-16 17:59:49 -0500

    Remove debugging

--- web.rb
+++ web.rb
@@ -4,7 +4,6 @@ require './responses.rb'
 post '/' do
   request.body.rewind
   json = JSON.parse request.body.read
-  firstResponder(json)  #|| "foo" ||  {}.to_json
-  #return json.to_json
-
+  firstResponder(json)  
+  
 end
