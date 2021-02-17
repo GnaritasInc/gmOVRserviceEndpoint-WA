b2e470c2521af741f47372b527c49983f75da133
Christopher <cwisdo@gnaritas.com>
2021-02-17 14:02:11 -0500

    Store request and response

--- web.rb
+++ web.rb
@@ -4,6 +4,14 @@ require './responses.rb'
 post '/' do
   request.body.rewind
   json = JSON.parse request.body.read
-  firstResponder(json)  
+  api_response=firstResponder(json) 
+  Post.create!(request: json, response:api_response) 
+  return api_response
+
   
+end
+
+get "/" do
+  @posts = Post.order("created_at DESC")
+  erb :"posts/index"
 end
